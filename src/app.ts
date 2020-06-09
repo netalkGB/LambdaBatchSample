d5a22be1edfb251278887978bb5348d9c060e56c
netalkGB <13611993+netalkGB@users.noreply.github.com>
2020-06-10 00:48:37 +0900

    どちらのモードかを表示

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 exports.lambdaHandler = async (event: any, context: any) => {
+    if (process.env['NODE_ENV'] !== 'production') {
+        console.log('running on development mode.')
+    } else {
+        console.log('running on production mode.')
+    }
     const response = 'test'
     return response
 }
